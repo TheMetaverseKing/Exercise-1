ca6468e7a206ec619068d2527ddc22d245eb152c
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-07-31 00:35:52 +0500

    Add files via upload

---
+++ Ex1.js
@@ -0,0 +1,4 @@
+"use strict";
+let personename = "Najeeb";
+let message = `Hello ${personename}, You are going to be the King of Metaverse. The Un-Disputed Champion of all times.`;
+console.log(message);
---
+++ Ex1.ts
@@ -0,0 +1,3 @@
+let personename:string = "Najeeb";
+let message:string = `Hello ${personename}, You are going to be the King of Metaverse. The Un-Disputed Champion of all times.`
+console.log(message);
